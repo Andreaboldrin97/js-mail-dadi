0a0f96f2eb871d3dcdaaf4af90fa2f76d27fdb1b
Andreaboldrin97 <boldrinandrea23@gmail.com>
2022-06-17 15:22:42 +0200

    Create var randomN for user and pc

---
+++ jsdadi/main.js
@@ -0,0 +1,8 @@
+//creo una var per il numero del pc
+let pcNumber = Math.floor((Math.random() * 6) + 1);
+console.log(pcNumber)
+
+//?creo una var per il numero del user
+let userNumber = Math.floor((Math.random() * 6) + 1);
+console.log(userNumber)
+
