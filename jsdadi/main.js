bffdfc7a7fe9b91dd49c956ede80b937b6a918b6
Andreaboldrin97 <boldrinandrea23@gmail.com>
2022-06-17 15:29:14 +0200

    created if with comparison of values

--- jsdadi/main.js
+++ jsdadi/main.js
@@ -5,4 +5,14 @@ console.log(pcNumber)
 //?creo una var per il numero del user
 let userNumber = Math.floor((Math.random() * 6) + 1);
 console.log(userNumber)
-
+//! creo unistruzione condizionale 
+//*se pcNumber > userNumber allora il pc vince
+if( pcNumber > userNumber){
+    console.log(`mi dispiace amico hai perso`)
+    //*se pcNumber < userNumber allora il user vince
+}else if( pcNumber < userNumber){
+    console.log(`WoW amico , mi hai batutto che fortuna`)
+    //*se pcNumber = userNumber allora il pc vince 
+}else{
+    console.log(`mi dispiace amico ma con un pareggio il banco vince sempre`)
+}
